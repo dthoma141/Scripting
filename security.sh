4d0fe2462d057c435718deb190f8ea906cf4eca7
dthoma141 <derekmthoma@gmail.com>
2017-08-14 21:23:33 -0600

    3

---
+++ security.sh
@@ -0,0 +1,28 @@
+#!bin/bash
+
+case "$1" in
+-h)
+    # Nothing needs done
+    echo -n "-se for senlinx"
+    echo -n '-fd for firewall d'
+    ;;
+
+-se)
+    echo '0 to disable selinux till reboot'
+    echo '1 to enable selinux till reboot'
+    read choice; echo 'please make a choice:'
+    case "$choice" in
+    0)
+        sestatus
+        setstatus 0
+        sestatus
+        ;;
+
+    1)
+        sestatus
+        setstatus 1
+        sestatus
+        ;;
+    esac
+    ;;
+esac
