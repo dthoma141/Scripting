d62dbbae1714ef29d8886c7306814296a9aa5938
dthoma141 <derekmthoma@gmail.com>
2017-08-16 20:15:12 -0600

    3

--- security.sh
+++ security.sh
@@ -8,6 +8,7 @@ case "$1" in
     ;;
 
 -se)
+    # This currently will not survice reboot.
     echo '0 to disable selinux till reboot'
     echo '1 to enable selinux till reboot'
     sestatus
@@ -22,6 +23,32 @@ case "$1" in
         setenforce 1
         sestatus
         ;;
+    *)
+        echo 'Thats bad imput :( I have to leave now'
+        exit
     esac
     ;;
+
+-SE)
+    # This currently will not survice reboot.
+    echo '0 to disable selinux till reboot'
+    echo '1 to enable selinux till reboot'
+    getenforce
+    read choice; echo 'please make a choice:'
+    case "$choice" in
+    0)
+        setenforce 0
+        getenforce
+        ;;
+
+    1)
+        setenforce 1
+        getenforce
+        ;;
+    *)
+        echo 'Thats bad imput :( I have to leave now'
+        exit
+      esac
+    ;;
+
 esac
